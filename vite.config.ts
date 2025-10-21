cb2169e0ec4a568f504c1fa834be5ec2942cfcb7
sergemezui <sergemezui.dev@gmail.com>
2025-10-21 21:59:00 +0100

    feat: remove unused files and configurations for a cleaner project structure

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import biomePlugin from "vite-plugin-biome";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-	plugins: [tailwindcss(), tsconfigPaths(), biomePlugin()],
-});
