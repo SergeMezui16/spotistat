fb32d39a914b7f97139e969150820408478eecc0
sergemezui <sergemezui.dev@gmail.com>
2025-10-22 15:33:22 +0100

    feat: add react-query for data fetching, implement SpotifyProvider and ErrorBoundary in App component, and create useSpotify hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import { useAuth } from "@/hooks/use-auth";
 import Dashboard from "@/Dashboard";
+import { ErrorBoundary } from "react-error-boundary";
+import { useAuth } from "@/hooks/use-auth";
+import { SpotifyProvider } from "./providers/spotify-context";
 
 function App() {
-  const { accessToken, isLoading, login, error } = useAuth();
+	const { accessToken, isLoading, login, error, refreshToken } = useAuth();
 
-  if (isLoading) return <p>Loading authentication...</p>;
-  if (error) return <p>❌ {error}</p>;
+	if (error) return <p>❌ {error}</p>;
 
-  if (!accessToken) return <button type="button" onClick={login}>Login with Spotify</button>;
+	if (isLoading) return <p>Loading authentication...</p>;
+	if (!accessToken || !refreshToken)
+		return (
+			<button type="button" onClick={login}>
+				Login with Spotify
+			</button>
+		);
 
-  return <Dashboard accessToken={accessToken} />;
+	return (
+		<ErrorBoundary FallbackComponent={SpotifyErrorFallback}>
+			<SpotifyProvider refreshToken={refreshToken} accessToken={accessToken}>
+				<Dashboard />
+			</SpotifyProvider>
+		</ErrorBoundary>
+	);
 }
 
 export default App;
+
+function SpotifyErrorFallback({ error }: { error: Error }) {
+	return (
+		<div className="rounded bg-red-100 p-4 text-red-800">
+			<h2>⚠️ Spotify Error</h2>
+			<p>{error.message}</p>
+		</div>
+	);
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient();
 
 // biome-ignore lint/style/noNonNullAssertion: no need to check for null here
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>,
+);
