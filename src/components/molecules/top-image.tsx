3fb2602c0500588ef51cbd78b2c16f865faea6ec
sergemezui <sergemezui.dev@gmail.com>
2025-10-24 12:49:20 +0100

    fix(TopImage): adjust backdrop brightness for improved visibility; refactor BubbleBackground for theme-based color handling

--- src/components/molecules/top-image.tsx
+++ src/components/molecules/top-image.tsx
@@ -14,10 +14,11 @@ export const TopImage = ({
 		<div className="relative inline-block rounded">
 			<span
 				className={cn(
-					"absolute right-0 bottom-0 flex h-full w-full select-none items-end justify-end rounded font-extrabold text-5xl italic backdrop-brightness-50",
+					"absolute right-0 bottom-0 flex h-full w-full select-none items-end justify-end rounded font-extrabold text-5xl italic backdrop-brightness-75 dark:backdrop-brightness-50",
 					rank === 1 && "text-[#f9e125]",
 					rank === 2 && "text-[#d2c2c1]",
 					rank === 3 && "text-[#cd7f32]",
+					rank > 3 && "text-white"
 				)}
 			>
 				{rank}
