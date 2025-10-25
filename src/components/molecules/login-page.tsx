ed3c7f8dd50db8d244ba6e1ddf985598e3008d68
sergemezui <sergemezui.dev@gmail.com>
2025-10-25 09:51:45 +0100

    feat: add background to login page

--- src/components/molecules/login-page.tsx
+++ src/components/molecules/login-page.tsx
@@ -9,6 +9,7 @@ import {
 	EmptyTitle,
 } from "@/components/ui/empty";
 import { ScanFaceIcon } from "lucide-react";
+import { BubbleBackground } from "../ui/bubble-background";
 
 export const LoginPage = ({
 	login,
@@ -16,23 +17,26 @@ export const LoginPage = ({
 	login: ReturnType<typeof useAuth>["login"];
 }) => {
 	return (
-		<div className="flex h-screen w-screen items-center justify-center gap-4">
-			<Empty>
-				<EmptyHeader>
-					<EmptyMedia variant="icon">
-						<ScanFaceIcon />
-					</EmptyMedia>
-					<EmptyTitle>You're not logged.</EmptyTitle>
-					<EmptyDescription>
-						Spotify Top need you to login to your Spotify account and the
-						authorization to get data from your taste. Click on the button
-						bellow to start!
-					</EmptyDescription>
-				</EmptyHeader>
-				<EmptyContent>
-					<Button onClick={login}>Login with Spotify</Button>
-				</EmptyContent>
-			</Empty>
-		</div>
+		<BubbleBackground interactive>
+			<div className="z-50 flex h-screen w-screen items-center justify-center backdrop-blur-3xl">
+				<div className="mx-auto rounded-lg bg-card">
+				<Empty className="">
+					<EmptyHeader>
+						<EmptyMedia variant="icon">
+							<ScanFaceIcon />
+						</EmptyMedia>
+						<EmptyTitle>Hey, You're not logged.</EmptyTitle>
+						<EmptyDescription>
+							Spotify Top need you to login to your Spotify account and the
+							authorization to get data from your taste. Click on the button
+							bellow to start!
+						</EmptyDescription>
+					</EmptyHeader>
+					<EmptyContent>
+						<Button onClick={login}>Login with Spotify</Button>
+					</EmptyContent>
+				</Empty></div>
+			</div>
+		</BubbleBackground>
 	);
 };
